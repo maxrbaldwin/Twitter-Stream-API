e49bbdc5c8784689653fa89c8fa42b5f8d4021c8
Max Baldwin <maxrbaldwin2328@gmail.com>
2018-02-19 11:17:08 -0500

    Added id

--- streams/ids/index.js
+++ streams/ids/index.js
@@ -1,39 +1,46 @@
-var streamIDs = {
-    STREAM_IDS: [{
-        name: 'NYT',
-        id: 1255671
+const usStreamIds = {
+  area: 'us',
+  ids: [
+    {
+      name: 'NYT',
+      id: 1255671
     }, {
-        name: 'Guardian',
-        id: 87818409
+      name: 'bbcnews',
+      id: 612473
     }, {
-        name: 'bbcworld',
-        id: 742143
+      name: 'washingtonpost',
+      id: 2467791
+    } , {
+      name: 'newrepublic',
+      id: 82689705
     }, {
-        name: 'bbcnews',
-        id: 612473
-    }, {
-        name: 'washingtonpost',
-        id: 2467791
-    }, {
-        name: 'theatlantic',
-        id: 35773039
-    }, {
-        name: 'newrepublic',
-        id: 82689705
-    }, {
-        name: 'vice',
-        id: 23818581
-    }]
+      name: 'vice',
+      id: 23818581
+    }
+  ],
 };
 
-streamIDs.getStreamIDs = function() {
-    var ids = [];
-
-    this.STREAM_IDS.forEach(function(el, i) {
-        ids.push(el.id);
-    });
-
-    return ids.toString();
+const euStreamIds = {
+  area: eu,
+  ids: [
+    {
+      name: 'theatlantic',
+      id: 35773039,
+    },
+  ],
 }
 
-module.exports = streamIDs;
+function getStreamIds() {
+  const streams = [
+    usStreamIds,
+    euStreamIds
+  ];
+
+  streams.forEach(function(stream, i) {
+      ids.push(el.id);
+  });
+
+  return ids.toString();
+}
+
+module.exports.getStreamIDs = getStreamIds;
