e05455eb89363a791c421052a1fc663aae0eaac1
Max Baldwin <maxrbaldwin2328@gmail.com>
2018-02-23 11:49:49 -0500

    Scraper module

---
+++ src/utils/scraper.js
@@ -0,0 +1,26 @@
+const { head, get } = require('request').defaults({
+    maxRedirects: 100
+});
+
+function scrapeHead(url) {
+  return new Promise((resolve, reject) => {
+    head(url, {
+        followAllRedirects: true,
+    }, function(err, headResponse) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(headResponse);
+      }
+    });
+  })
+};
+
+function getUrlFromHead(headResponse) {
+  return (headResponse && headResponse.request) ? headResponse.request.href : null;
+};
+
+module.exports = {
+  scrapeHead,
+  getUrlFromHead,
+};
